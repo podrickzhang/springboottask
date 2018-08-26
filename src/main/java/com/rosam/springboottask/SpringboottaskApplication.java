42800a936331a49aa6480be6dfee873a12e07c71
podrickzhang <958786497@qq.com>
2018-08-26 23:16:09 +0800

    Initial commit

---
+++ src/main/java/com/rosam/springboottask/SpringboottaskApplication.java
@@ -0,0 +1,15 @@
+package com.rosam.springboottask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+//开启定时任务
+@EnableScheduling
+public class SpringboottaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboottaskApplication.class, args);
+	}
+}
